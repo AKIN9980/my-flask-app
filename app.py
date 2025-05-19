4b312585eba142275fcf87ecc35e57c0dc50eda1
AKIN9980 <programmer9980@gmail.com>
2025-05-19 14:25:03 +0300

    templates ve static klasörleri eklendi, css ve js bağlandı

--- app.py
+++ app.py
@@ -1,43 +1,10 @@
-from flask import Flask, render_template_string
-import random
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-messages = [
-    "YOU ARE IDIOT!",
-    "SORRY, NO INTELLIGENCE DETECTED.",
-    "TRY AGAIN, GENIUS.",
-    "ERROR 404: SMARTNESS NOT FOUND.",
-    "DON'T TAKE IT PERSONALLY!"
-]
-
 @app.route('/')
-def home():
-    message = random.choice(messages)
-    return render_template_string('''
-    <html>
-    <head>
-      <title>youareidiot</title>
-      <style>
-        body {
-          background-color: black;
-          color: red;
-          font-family: 'Arial Black', Arial, sans-serif;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          height: 100vh;
-          margin: 0;
-          font-size: 5em;
-          user-select: none;
-        }
-      </style>
-    </head>
-    <body>
-      {{ message }}
-    </body>
-    </html>
-    ''', message=message)
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
