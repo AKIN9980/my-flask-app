559b5bb8ff970a73a880d12f2652744815d4fb69
AKIN9980 <programmer9980@gmail.com>
2025-05-19 15:04:28 +0300

    sistem güncellemesi666

--- app.py
+++ app.py
@@ -1,20 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session
 from flask_socketio import SocketIO, emit
-import random
+from flask_session import Session
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+app.secret_key = 'gizli-anahtar'
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
+socketio = SocketIO(app, manage_session=False)
 
-@app.route("/")
+messages = []
+
+@app.route('/')
 def index():
-    return render_template("index.html")
+    if 'nickname' not in session:
+        return render_template('nickname.html')
+    return render_template('chat.html', nickname=session['nickname'])
 
-@socketio.on("message")
+@app.route('/set-nickname', methods=['POST'])
+def set_nickname():
+    nickname = request.form.get('nickname')
+    if nickname:
+        session['nickname'] = nickname
+        return ('', 204)
+    return ('Bad Request', 400)
+
+@socketio.on('connect')
+def on_connect():
+    emit('load_messages', messages)
+
+@socketio.on('send_message')
 def handle_message(data):
-    username = data.get("username")
-    msg = data.get("message")
-    emit("message", {"username": username, "message": msg}, broadcast=True)
+    msg = {
+        'nickname': session.get('nickname', 'Anonim'),
+        'text': data['text']
+    }
+    messages.append(msg)
+    emit('receive_message', msg, broadcast=True)
 
-if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    socketio.run(app)
 
