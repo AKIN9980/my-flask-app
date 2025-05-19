85834d6135251a4902652ebf962eba468e94ccb8
AKIN9980 <programmer9980@gmail.com>
2025-05-19 14:14:17 +0300

    app.py güncellendi

--- app.py
+++ app.py
@@ -1,9 +1,43 @@
-from flask import Flask
+from flask import Flask, render_template_string
+import random
+
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "<h1>Merhaba freakgpt! Bu site Render'da çalışıyor.</h1>"
+messages = [
+    "YOU ARE IDIOT!",
+    "SORRY, NO INTELLIGENCE DETECTED.",
+    "TRY AGAIN, GENIUS.",
+    "ERROR 404: SMARTNESS NOT FOUND.",
+    "DON'T TAKE IT PERSONALLY!"
+]
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+@app.route('/')
+def home():
+    message = random.choice(messages)
+    return render_template_string('''
+    <html>
+    <head>
+      <title>youareidiot</title>
+      <style>
+        body {
+          background-color: black;
+          color: red;
+          font-family: 'Arial Black', Arial, sans-serif;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 100vh;
+          margin: 0;
+          font-size: 5em;
+          user-select: none;
+        }
+      </style>
+    </head>
+    <body>
+      {{ message }}
+    </body>
+    </html>
+    ''', message=message)
+
+if __name__ == '__main__':
+    app.run(debug=True)
