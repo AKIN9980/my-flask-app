2a8aebc04fd4eafe7fd3d415380eae2c9b801a80
AKIN9980 <programmer9980@gmail.com>
2025-05-19 19:39:39 +0300

    voiceall

--- app.py
+++ app.py
@@ -190,6 +190,14 @@ def handle_message(data):
     messages.append(full_msg)
     emit('message', {'msg': full_msg, 'admin': False}, broadcast=True)
 
+@socketio.on('ready-for-voice')
+def ready_for_voice(data):
+    emit('ready-for-voice', data, broadcast=True, include_self=False)
+
+@socketio.on('signal')
+def signal(data):
+    emit('signal', data, broadcast=True, include_self=False)
+
 if __name__ == '__main__':
     socketio.run(app, debug=True)
 
